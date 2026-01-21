4d14ada7efbb1b04b97862106569892ff09e8598
WangZhen-Ryan <15000059524@163.com>
2026-01-21 17:31:07 +1100

    cache fetching and UI when the apiBase URL

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/SmartElectricity/",
+  base: command === "build" ? "/SmartElectricity/" : "/",
   server: {
     proxy: {
       "/api": "http://localhost:5174",
     },
   },
-});
+}));
